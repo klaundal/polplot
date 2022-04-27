79129860d0625cc1b9d8b13c7e59ce18b1ba0271
Jone P. Reistad <jone@c02f9621md6r.klientdrift.uib.no>
2022-04-27 14:57:28 +0200

    Added a few lines to _fast_bin(), mainly documentation, that got lost in the move from DAG repo

--- grids.py
+++ grids.py
@@ -10,14 +10,14 @@ def equal_area_grid(dr = 2, K = 0, M0 = 8, N = 20):
         The latitudinal resolution of the grid. Default 2 degrees
     K : int (optional)
         This number determines the colatitude of the inner radius of the
-        post poleward ring of grid cells (the pole is not inlcluded!). 
+        post poleward ring of grid cells (the pole is not inlcluded!).
         It relates to this colatitude (r0) as r0/dr = (2K + 1)/2 => K = (2r0/dr - 1)/2.
         Default value is 0
     M0 : int (optional)
         The number of sectors in the most poleward ring. Default is 8
     N : int (optional)
-        The number of rings to be included. This determiend how far 
-        equatorward the grid extends. Typically if dr is changed from 2 to 1, 
+        The number of rings to be included. This determiend how far
+        equatorward the grid extends. Typically if dr is changed from 2 to 1,
         N should be doubled to reach the same latitude. Default is 20, which means
         that the equatorward edge of the grid is 89 - 20*2 = 49 degrees
         (the most poleward latitude is 89 with default values)
@@ -31,11 +31,11 @@ def equal_area_grid(dr = 2, K = 0, M0 = 8, N = 20):
         Array of magnetic local times for the equatorward west ("lower left") corner
         of the grid cells.
     mltres : array
-        width, in magnetic local time, of the cells with lower left corners described 
-        by mlat and mlt. Notice that this width changes with latitude, while the 
+        width, in magnetic local time, of the cells with lower left corners described
+        by mlat and mlt. Notice that this width changes with latitude, while the
         latitudinal width is fixed, determined by the `dr` parameter
 
-    
+
     """
 
     r0 = dr * (2*K + 1)/2.
@@ -52,7 +52,7 @@ def equal_area_grid(dr = 2, K = 0, M0 = 8, N = 20):
         M = M *  (1 + 1./(K + i + 1.)) # this is the partion for i + 1
 
         grid[90 - (r0 + (i + 1)*dr) - dr] = np.linspace(0, 24 - 24./M, int(M)) # these are the lower limits in MLT
-        # jreistad added int() to the M argument in np.linspace call in the above line. 
+        # jreistad added int() to the M argument in np.linspace call in the above line.
         # I think line 43 forces this to always be the case. (2020-06-18)
 
     mlats = []
@@ -170,7 +170,7 @@ def bin_number_OLD(grid, mlat, mlon, mlt = True):
             iii     = np.where( (grid[0] == unique_mlats[mlatbin]) )[0]
             ilon    = np.argmin(np.abs(grid[1][iii] - mlon[i]))
             res[i]  = iii[ilon]
-    
+
     return res
 
 
@@ -196,13 +196,13 @@ def bin_number(grid,mlat,mlon,
 
     MODIFICATIONS
     SMH 2021/04/10 Added fortran routines in bin_number_fortran.f95. Run "make" in ${DAGDIR}/src/pysymmetry/visualization directory.
-    SMH 2021/04/15 Added Kalle's wildly fast "_fast_bin" routine. It slays my fortran code by as much as a factor of 40, and the 
+    SMH 2021/04/15 Added Kalle's wildly fast "_fast_bin" routine. It slays my fortran code by as much as a factor of 40, and the
                    original bin_number by a factor of 200.
     """
-    
+
     mlat = mlat.flatten()
     mlon = mlon.flatten()
-    
+
     if version_binongrid in ['v1','v2']:
         try:
             from pysymmetry.visualization.binnumberFORTRAN import binongrid,binongrid2
@@ -222,7 +222,7 @@ def bin_number(grid,mlat,mlon,
 
     assert grid.shape[0] == 2
     assert len(mlat) == len(mlon)
-    
+
     res = np.zeros(mlat.size,dtype=np.int64)-1
 
     if mlt: mlon = mlon * 15
@@ -239,14 +239,14 @@ def bin_number(grid,mlat,mlon,
         res = binongrid2(mlat,mlon,grid[0],grid[1],dlat,mlonres)
     else:
         for ibin,(tmpmlon,tmpdmlon,tmpmlat) in enumerate(zip(grid[1], mlonres,grid[0])):
-        
+
             tmpi = np.where((mlat >= tmpmlat) & (mlat < (tmpmlat+dlat[ibin]+1e-7)) & (mlon >= tmpmlon) & (mlon < (tmpmlon+tmpdmlon+1e-7)))[0]
             if verbose:
                 print(f"{ibin:02d}, {tmpmlat:5.2f}, {tmpmlat+dlat[ibin]:5.2f}, {tmpmlon:5.2f}, {tmpmlon+tmpdmlon:5.2f}, {tmpi.size:05d}")
             res[tmpi] = ibin
 
     if version_binongrid in ['v1','v2']:
-        res = res - 1 
+        res = res - 1
 
     return res
 
@@ -254,34 +254,47 @@ def bin_number(grid,mlat,mlon,
 def _fast_bin(grid, mlat, mlon,
              mlt=True):
     """
-    Kalle's ultra-fast binning routine, modified so for full compatibility with 
+    Kalle's ultra-fast binning routine, modified so for full compatibility with
     calls to the original bin_number routine using version_binongrid='v3'.
 
-    SMH 2021/04/15 
+    According to Kalle (author), the grid object passed to this function needs
+    to be made by the sdarngrid function above.
+
+    MLT keyword refer to the mlon argument only, since the sdarngrid function
+    return the longitude in degrees.
+
+    Locations below the defined grid are set to -1 in the returned array.
+
+    SMH 2021/04/15
+    Modified by JPR 2021/10/20
     """
-    
-    mlon = mlon * 15 if mlt is True else mlt
+
+    mlon = mlon * 15 if mlt is True else mlon
     llat = np.unique(grid[0]) # latitude circles
     assert np.allclose(np.sort(llat) - llat, 0) # should be in sorted order automatically. If not, the algorithm will not work
     dlat = np.diff(llat)[0] # latitude step
     latbins = np.hstack(( llat, llat[-1] + dlat )) # make latitude bin edges
     latbin_n = np.digitize(mlat, latbins) - 1 # find the latitude index for each data point
-    
+
     # number of longitude bins in each latitude ring:
     nlons = np.array([len(np.unique(grid[1][grid[0] == lat])) for lat in llat])
-    
+
     # normalize all longitude bins to the equatorward ring:
     _lon = mlon * nlons[latbin_n] / nlons[0]
-    
+
     # make longitude bin edges for the equatorward ring:
     llon = np.unique(grid[1][grid[0] == llat[0]])
     dlon = np.diff(llon)[0]
     lonbins = np.hstack((llon, llon[-1] + dlon)) # make longitude bin edges
     lonbin_n = np.digitize(_lon, lonbins) - 1 # find the longitude bin
-    
+
     # map from 2D bin numbers to 1D by adding the number of bins in each row equatorward:
     bin_n = lonbin_n + np.cumsum(np.hstack((0, nlons)))[latbin_n]
-    
+
+    #Set the bin number of outside grid observations to -1
+    outside = bin_n == grid.shape[1]
+    bin_n[outside] = -1
+
     return bin_n
 
 
@@ -309,7 +322,7 @@ def get_sdarngrid_mlon_resolution(grid):
         mlonzeros = mlonzeros[:-1]
     stopmlt = np.append(mlonzeros[1:],grid[1].shape[0])
     mlonres = np.zeros(grid[1].shape)
-    
+
     if DEBUG:
         print("{:10s}, {:10s}, {:10s}".format("startmlt","stopmlt","startmlat"))
     for istart in range(len(mlonzeros)):
@@ -318,16 +331,16 @@ def get_sdarngrid_mlon_resolution(grid):
                   format(grid[1][mlonzeros[istart]],
                          grid[1][mlonzeros[istart]+1],
                          forstermlat[mlonzeros[istart]]))
-    
+
         mlonres[mlonzeros[istart]:stopmlt[istart]] = grid[1][mlonzeros[istart]+1]-grid[1][mlonzeros[istart]]
 
     return mlonres
 
 #%%
-def cell_area(mlat, r, mlatres):    
+def cell_area(mlat, r, mlatres):
     '''
     Calculates the cell area of grid cells on a sphere
-    
+
     Parameters
     ----------
     mlat    : Array of floats
@@ -343,19 +356,19 @@ def cell_area(mlat, r, mlatres):
         Area of the cells in unit of r
 
     '''
-    
+
     if not (isinstance(r, int) or isinstance(r, float)):
         raise TypeError('r has to be interger or float')
-    
+
     if not (isinstance(mlatres, int) or isinstance(mlatres, float)):
         raise TypeError('mlatres has to be interger or float')
-    
+
     if not (isinstance(mlat, list) or isinstance(mlat, np.ndarray)):
         raise TypeError('mlat has to be an 1D list of array')
-    
+
     if isinstance(mlat, list):
         mlat = np.array(mlat)
-    
+
     # Number of cells per latitude band
     p_per_lat = np.zeros(len(mlat))
     unique_lats, counts = np.unique(mlat, return_counts=True)
@@ -369,4 +382,3 @@ def cell_area(mlat, r, mlatres):
     cell_area = 2*np.pi*r*h/p_per_lat
 
     return cell_area
-
